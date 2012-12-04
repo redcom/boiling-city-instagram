1ad0c0206051ca30ae1a04cd72171e27252bee40
Razvan Moraru <razvan@widgets.ro>
2012-12-04 14:09:02 +0300

    Removing configs for sharing on github

--- server/config.js
+++ server/config.js
@@ -1,34 +1,29 @@
 module.exports = function(app) {
-    var dublinCoverage=[
+    var domainName = 'http://';
+    // subscriptions for geo location and for tags
+    var cityCoverage=[
     //dublin
         { lat: 53.3494, lng:-6.2601 },
         { lat: 53.3731, lng: -6.324 },
-        //{ lat: 53.305,  lng: -6.2457 },
-        //{ lat: 53.3825, lng: -6.2251 },
-        //{ lat: 53.2964, lng: -6.1379 },
+        { lat: 53.305,  lng: -6.2457 },
+        { lat: 53.3825, lng: -6.2251 },
+        { lat: 53.2964, lng: -6.1379 },
         { lat: 53.393,  lng: -6.1321 },
         { lat: 53.4288, lng: -6.2766 }
     ];
     var tagSubscriptions = ['dublinedge'];
-    var callbackURL = 'http://boiling-city-instagram.herokuapp.com/instagram/cb';
-    var instagramConfig = {
-        clientId: '84246d45a2f24951b661a717d130c57e',
-        clientSecret: 'ac63a198d3a54e5194b3375a1fb21589'
 
-        //clientId: '32b55f2c330e479a9e4ccb401d935c7d',
-        //clientSecret: '38ac13830cd24499bf9a5276b3d98704'
+    var callbackURL = domainName+'/instagram/cb';
+    var instagramConfig = {
+        clientId: 'YOUR_CLIENT_ID',
+        clientSecret: 'YOUR_CLIENT_SECRET'
     };
 
-    app.set('cityCoverege', dublinCoverage);
-    app.set('callbackURL', callbackURL);
-    app.set('instagramConfig', instagramConfig);
-
     var config = {
-        cityCoverage: dublinCoverage,
+        cityCoverage: cityCoverage,
         instagramConfig: instagramConfig,
         tagSubscriptions: tagSubscriptions,
         callbackURL: callbackURL
-
     };
     return config;
 };
